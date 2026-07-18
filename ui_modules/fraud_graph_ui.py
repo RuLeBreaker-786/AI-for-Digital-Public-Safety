dbf359de631e023839a9ec214f948ff9b42da4e5
Ramesh Saini <174591106+RuLeBreaker-786@users.noreply.github.com>
2026-07-18 18:45:33 +0530

    Update fraud_graph_ui.py

--- ui_modules/fraud_graph_ui.py
+++ ui_modules/fraud_graph_ui.py
@@ -1,8 +1,9 @@
+import os
 import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 
 def render_fraud_graph_module():
